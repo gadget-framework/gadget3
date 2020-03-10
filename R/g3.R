9b3fe30d063d73f5448940505e7562e81b906174
Jamie Lentin <jm@lentin.co.uk>
2020-03-10 16:39:53 +0000

    Manage stocks with more realistic array types

--- R/g3.R
+++ R/g3.R
@@ -8,6 +8,7 @@ g3a_time <- function(start_year, end_year, steps = c(12)) {
     total_steps <- ~length(steps) * (end_year - start_year)
 
     list(step0 = ~{
+        comment("g3a_time")
         cur_time <- cur_time + 1
         cur_step_len <- 55 # TODO: More realistic definition, can we remove if not required?
         if (cur_time < total_steps) break
@@ -29,9 +30,10 @@ stock_extend <- function(inner, ...) {
 
 stock_step <- function(stock, f, init = quote({}), final = quote({})) {
     # Wrap f in the stock's iterate code
-    f <- f_substitute(stock$iterate, list(
+    f <- f_substitute(f, list(
         stock_num = stock$stock_num,
-        stock_wgt = stock$stock_wgt,
+        stock_wgt = stock$stock_wgt))
+    f <- f_substitute(stock$iterate, list(
         extension_point = f))
     f <- f_substitute(~{
         comment(stock_comment)
@@ -60,8 +62,8 @@ stock_definition <- function(stock, var_name) {
 
 # TODO: Using this directly on top of others won't produce valid code. Should they be collapsed together?
 g3_stock <- function(stock_name, stock_lengths) {
-    stock_num <- rep(0, length(stock_lengths))  # TODO: These need to expand with each addition
-    stock_wgt <- rep(0, length(stock_lengths))
+    stock_num <- array(dim = c(length(stock_lengths)))
+    stock_wgt <- array(dim = c(length(stock_lengths)))
     stock_lenmid <- vapply(seq_along(stock_lengths), function (i) {
         if (i < length(stock_lengths)) {
             mean(c(stock_lengths[[i]], stock_lengths[[i+1]]))
@@ -72,10 +74,9 @@ g3_stock <- function(stock_name, stock_lengths) {
 
     list(
         name = stock_name,
-        stock_num = as.symbol("stock_num"),
-        stock_wgt = as.symbol("stock_wgt"),
+        stock_num = parse(text = "stock_num[]")[[1]],
+        stock_wgt = parse(text = "stock_wgt[]")[[1]],
         stock_name = stock_name,
-        lengths = stock_lengths,
         capture = ~{stock_lengths <- length_agg},
         iterate = ~extension_point)
 }
@@ -88,18 +89,22 @@ g3s_fleet <- function(stock_name) {
 }
 
 g3s_livesonareas <- function(inner_stock, stock_areas) {
+    stock_num <- array(dim = c(dim(stock_definition(inner_stock, 'stock_num')), length(stock_areas)))
+    stock_wgt <- array(dim = c(dim(stock_definition(inner_stock, 'stock_wgt')), length(stock_areas)))
     stock_extend(inner_stock,
-        stock_num = call("[[", inner_stock$stock_num, as.symbol("area")),
-        stock_wgt = call("[[", inner_stock$stock_wgt, as.symbol("area")),
+        stock_num = as.call(c(as.list(inner_stock$stock_num), as.symbol("area"))),
+        stock_wgt = as.call(c(as.list(inner_stock$stock_wgt), as.symbol("area"))),
         capture = f_substitute(~if (area %in% stock_areas) extension_point, list()),
         iterate = f_substitute(~for (area in stock_areas) extension_point, list(extension_point = inner_stock$iterate))
     )
 }
 
 g3s_age <- function(inner_stock, stock_ages) {
+    stock_num <- array(dim = c(dim(stock_definition(inner_stock, 'stock_num')), length(stock_ages)))
+    stock_wgt <- array(dim = c(dim(stock_definition(inner_stock, 'stock_wgt')), length(stock_ages)))
     inner_stock <- stock_extend(inner_stock,
-        stock_num = call("[[", inner_stock$stock_num, as.symbol("age")),
-        stock_wgt = call("[[", inner_stock$stock_wgt, as.symbol("age")),
+        stock_num = as.call(c(as.list(inner_stock$stock_num), as.symbol("age"))),
+        stock_wgt = as.call(c(as.list(inner_stock$stock_wgt), as.symbol("age"))),
         capture = f_substitute(~for (age in stock_ages) extension_point, list()),
         iterate = f_substitute(~for (age in stock_ages) extension_point, list(extension_point = inner_stock$iterate)))
 }
@@ -141,15 +146,15 @@ g3a_grow_impl_bbinom <- function (beta, maxlengthgroupgrowth) {
 }
 
 g3a_grow <- function(stock, growth_fn, impl_fn) {
-    delt_l <- "TODO: vector lengthgroups long"
     stock_growth_num <- stock_definition(stock, 'stock_num')
-    stock_growth_ratio <- matrix(nrow = length(stock$lengths), ncol = length(stock$lengths))
+    stock_delt_l <- array(dim = dim(stock_growth_num)[[1]])
+    stock_growth_ratio <- matrix(nrow = length(stock_delt_l), ncol = length(stock_delt_l))
     list(
         step055b = stock_step(stock,
             init = stock_growth_num ~ 0,
             final = stock_num ~ stock_growth_num,
             f_substitute(~{
-                delt_l <- growth_fn
+                stock_delt_l <- growth_fn
                 stock_growth_ratio <- impl_fn
 
                 stock_growth_num <- stock_num * stock_growth_ratio
@@ -202,6 +207,9 @@ g3_compile <- function(steps) {
                 defn <- call("<-", as.symbol(var_name), call('$', as.symbol("data"), var_val))
             } else if ('g3_param' %in% class(var_val)) {
                 defn <- call("<-", as.symbol(var_name), call('$', as.symbol("param"), var_val))
+            } else if (is.array(var_val) && all(is.na(var_val))) {
+                # Just define dimensions
+                defn <- call("<-", as.symbol(var_name), substitute(array(dim = x), list(x = dim(var_val))))
             } else if (is.array(var_val)) {
                 # Generate code to define matrix
                 defn <- call("<-", as.symbol(var_name), parse(text = deparse(var_val))[[1]])
