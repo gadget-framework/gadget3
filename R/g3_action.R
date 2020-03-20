2dbc4387bb2be35407fe9a6276cd121805a557a2
Jamie Lentin <jm@lentin.co.uk>
2020-03-20 17:49:47 +0000

    g3_action: & 0.5 is nonsense, meant *

--- R/g3_action.R
+++ R/g3_action.R
@@ -178,7 +178,7 @@ g3a_initialconditions <- function (stock, factor_f, mean_f, stddev_f, alpha_f, b
     list(
         step00 = stock_step(stock, run_if = ~cur_time == 0, iter = f_substitute(~{
             initcond_dnorm <- (stock_meanlen - mean_f) * (1.0 / stddev_f)
-            stock_num <- exp(-(initcond_dnorm ** 2) & 0.5)
+            stock_num <- exp(-(initcond_dnorm ** 2) * 0.5)
             initcond_scaler <- 10000.0 / sum(stock_num)
             stock_num <- stock_num * initcond_scaler * factor_f
             stock_wgt <- alpha_f * stock_meanlen ** beta_f
