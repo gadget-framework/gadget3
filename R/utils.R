28ffe61320235bd63676ee2396b835310730a5d0
Jamie Lentin <jm@lentin.co.uk>
2020-03-31 16:47:21 +0100

    debug_utils: Move development tools to debug_utils
    These aren't used in the package proper, just handy tools

--- R/utils.R
+++ R/utils.R
@@ -1,5 +1,3 @@
-library(R6)
-
 # Add missing is.formula
 is.formula <- function (o) {
     is.call(o) && 'formula' %in% class(o)
@@ -10,59 +8,9 @@ call_to_formula <- function (c, env = parent.frame()) {
     formula(call("~", c), env = env)
 }
 
-# Display the parse tree of a formulae / language object
-parse_tree <- function (o, prefix = "") {
-    short_str <- function (x) {
-        trimws(capture.output(str(x))[[1]])
-    }
-
-    if (missing(o)) {
-        # Missing placeholder
-        writeLines(paste0(prefix, " (missing)"))
-    } else if ("formula" %in% class(o)) {
-        writeLines(paste0(prefix, "Formula: ", if (length(o) > 2) o[[2]] else ""))
-        parse_tree(o[[length(o)]], prefix = prefix)
-
-        envir <- attr(o, '.Environment')
-        while (!(environmentName(envir) %in% c("R_GlobalEnv", "R_EmptyEnv"))) {
-            writeLines(paste0(prefix, "With environment:"))
-            writeLines(paste0(
-                prefix, " * ",
-                ls(envir),
-                " = ",
-                vapply(ls(envir), function (n) short_str(get(n, envir)), character(1)),
-                ""))
-            envir <- parent.env(envir)
-        }
-    } else if (is.symbol(o) || !is.language(o)) {
-        # Write out symbol / number / ... (NB: symbols are also language)
-        writeLines(paste0(prefix, short_str(o)))
-    } else {
-        # Recurse over language object as list
-        for (inner in as.list(o)) {
-            parse_tree(inner, prefix = paste0(prefix, "| "))
-        }
-    }
-}
-# parse_tree(~{for (x in c(1,2,3)) {str(x) ; str(x+1)} })
-
-envir_tree <- function (o) {
-    if (is.formula(o)) return(envir_tree(f_envir(o)))
-
-    out <- as.list(o)
-    if (!(is_base_envir(parent.env(o)))) {
-        attr(out, '_parent') <- envir_tree(parent.env(o))
-    }
-    return(out)
-}
-
 # Pull out a formula's environment
 f_envir <- function (f) {
     attr(f, '.Environment')
-}
-
-is_base_envir <- function (env) {
-    environmentName(env) %in% c("R_GlobalEnv", "R_EmptyEnv")
 }
 
 # Replace the target of this formulae with what we really want.
