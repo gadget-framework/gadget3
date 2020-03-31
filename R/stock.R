d32ffc3c168d356c8e400802cd03507a3a9a4d6e
Jamie Lentin <jm@lentin.co.uk>
2020-03-31 16:57:54 +0100

    stock: Combine areas.R and stock.R

--- R/stock.R
+++ R/stock.R
@@ -16,6 +16,29 @@ stock_definition <- function(stock, var_name) {
     get(var_name, envir = f_envir(stock$iterate))
 }
 
+# A vector of areas, subsetting preserves the full list
+g3_areas <- function (...) {
+    structure(
+        seq_along(c(...)),
+        names = c(...),
+        all_areas = c(...),
+        class = 'g3_areas')
+}
+
+`[.g3_areas` <- function (x, ..., drop = FALSE) {
+    y <- NextMethod("[")
+    attr(y, "all_areas") <- attr(x, "all_areas")
+    class(y) <- oldClass(x)
+    return(y)
+}
+
+`[[.g3_areas` <- function (x, ..., drop = FALSE) {
+    y <- NextMethod("[[")
+    attr(y, "all_areas") <- attr(x, "all_areas")
+    class(y) <- oldClass(x)
+    return(y)
+}
+
 # TODO: Using this directly on top of others won't produce valid code. Should they be collapsed together?
 g3_stock <- function(stock_name, minlength, maxlength, dl) {
     # If these are literals, they should be integers
