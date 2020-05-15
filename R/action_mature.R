ad9aede13376e7ceb9cd40c4a67cdffc2bb52c23
Jamie Lentin <jm@lentin.co.uk>
2020-05-15 11:41:01 +0100

    action_mature: Better comments

--- R/action_mature.R
+++ R/action_mature.R
@@ -15,12 +15,13 @@ g3a_mature <- function(input_stock, output_stocks, maturity_f, maturity_steps = 
 
     out <- new.env(parent = emptyenv())
     out$step056 <- stock_step(input_stock, extra_stock = matured, iter_f = f_substitute(~{
-        comment("Extract matured stock")
+        comment(stock_comment)
         matured__num[matured__iter] <- input_stock__num[input_stock__iter] * maturity_f
         input_stock__num[input_stock__iter] <- input_stock__num[input_stock__iter] - matured__num[matured__iter]
         matured__wgt[matured__iter] <- input_stock__wgt[input_stock__iter] * maturity_f
         input_stock__wgt[input_stock__iter] <- input_stock__wgt[input_stock__iter] - matured__wgt[matured__iter]
     }, list(
+        stock_comment = paste0("Move matured ", input_stock$name ," into temporary storage"),
         maturity_f = maturity_f)))
 
     for (n in seq_len(nrow(output_stocks))) {
@@ -28,10 +29,11 @@ g3a_mature <- function(input_stock, output_stocks, maturity_f, maturity_steps = 
         output_ratio <- output_stocks$ratios[[n]]
 
         assign(paste0('step075:', output_stock$name), stock_step(output_stock, extra_stock = matured, iter = f_substitute(~{
-            comment("Add matured stock into new stocks(s)")
+            comment(stock_comment)
             output_stock__num[output_stock__iter] <- output_stock__num[output_stock__iter] + matured__num[matured__iter] * output_ratio
             output_stock__wgt[output_stock__iter] <- output_stock__wgt[output_stock__iter] + matured__wgt[matured__iter] * output_ratio
         }, list(
+            stock_comment = paste0("Move matured ", input_stock$name ," to ", output_stock$name),
             output_ratio = output_ratio))), envir = out)
     }
 
