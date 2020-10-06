9670b038537bedd69c45e9bc56fef7c451cff971
Jamie Lentin <jm@lentin.co.uk>
2020-10-06 12:37:09 +0100

    action_report: Use aggregation to make simple in-memory reports

---
+++ R/action_report.R
@@ -0,0 +1,25 @@
+g3a_report_stock <- function (report_stock, input_stock, instance_name, run_f = TRUE, run_at = 11) {
+    out <- new.env(parent = emptyenv())
+
+    input_stock_instance_name <- paste0('input_stock__', instance_name)
+    report_stock_instance_name <- paste0('report_stock__', instance_name)
+    assign(report_stock_instance_name, stock_instance(report_stock))
+
+    out[[step_id(run_at, report_stock, instance_name)]] <- stock_step(f_substitute(~{
+        if (run_f) {
+            stock_comment("Fill in report ", report_stock, " from ", input_stock)
+            if (cur_time == 0L) {
+                stock_with(report_stock, report_instance[] <- 0)
+            }
+            stock_iterate(input_stock, stock_intersect(report_stock, {
+                stock_ss(report_instance) <- stock_ss(report_instance) + stock_ss(input_instance)
+            }))
+            stock_with(report_stock, g3_report(report_instance))
+        }
+    }, list(
+        input_instance = as.symbol(input_stock_instance_name),
+        report_instance = as.symbol(report_stock_instance_name),
+        run_f = run_f)))
+
+    return(as.list(out))
+}
---
+++ tests/test-action_report.R
@@ -0,0 +1,101 @@
+library(magrittr)
+library(unittest)
+
+library(gadget3)
+
+tmb_r_compare <- function (model_fn, model_tmb, params) {
+    if (nzchar(Sys.getenv('G3_TEST_TMB'))) {
+        # Reformat params into a single vector in expected order
+        par <- unlist(params[names(environment(model_cpp)$model_parameters)])
+        model_tmb_report <- model_tmb$report(par)
+        for (n in ls(environment(model_fn)$model_report)) {
+            ok(ut_cmp_equal(
+                as.vector(model_tmb_report[[n]]),
+                as.vector(environment(model_fn)$model_report[[n]]),
+                tolerance = 1e-5), paste("TMB and R match", n))
+        }
+    } else {
+        writeLines("# skip: not running TMB tests")
+    }
+}
+
+# Compare array by turning it back into a table first
+cmp_array <- function (ar, table_text) {
+    tbl <- read.table(
+        header = TRUE,
+        stringsAsFactors = FALSE,
+        colClasses = c(rep("character", length(dim(ar))), "numeric"),
+        text = table_text)
+    ut_cmp_identical(as.data.frame.table(ar, stringsAsFactors = FALSE), tbl)
+}
+
+prey_a <- g3_stock('prey_a', c(1)) %>% g3s_age(1, 5)
+
+# Report that aggregates ages together
+agg_report <- g3_stock('agg_report', c(1)) %>% 
+    g3s_agegroup(list(young = 1:3, old = 4:5)) %>%
+    g3s_time(year = 2000:2002)
+# Generate dissaggregated report by cloning the source stock, adding time
+raw_report <- g3s_clone(prey_a, 'raw_report') %>%
+    g3s_time(year = 2000:2002)
+
+actions <- list(
+    g3a_time(2000, 2002, steps = c(6, 6)),
+    g3a_initialconditions(prey_a, ~10 * age + prey_a__midlen * 0, ~100 * age + prey_a__midlen * 0),
+    g3a_age(prey_a),
+    g3a_report_stock(agg_report, prey_a, 'num'),
+    g3a_report_stock(raw_report, prey_a, 'num'),
+    list('999' = ~{ nll <- g3_param('x') }))
+params <- list(
+    x=1.0)
+            
+# Compile model
+model_fn <- g3_to_r(actions, trace = FALSE)
+# model_fn <- edit(model_fn)
+if (nzchar(Sys.getenv('G3_TEST_TMB'))) {
+    model_cpp <- g3_to_tmb(actions, trace = FALSE)
+    # model_cpp <- edit(model_cpp)
+    model_tmb <- g3_tmb_adfun(model_cpp, params)
+} else {
+    writeLines("# skip: not compiling TMB model")
+}
+
+ok_group("report", {
+    params <- list(
+        x=1.0)
+    result <- model_fn(params)
+    r <- environment(model_fn)$model_report
+    # str(result)
+    # str(as.list(r), vec.len = 10000)
+
+    ok(cmp_array(r$raw_report__num, "
+        length  age time Freq
+          len1 age1 2000   20
+          len1 age2 2000   40
+          len1 age3 2000   60
+          len1 age4 2000   80
+          len1 age5 2000  100
+          len1 age1 2001    0
+          len1 age2 2001   20
+          len1 age3 2001   40
+          len1 age4 2001   60
+          len1 age5 2001  180
+          len1 age1 2002    0
+          len1 age2 2002    0
+          len1 age3 2002   20
+          len1 age4 2002   40
+          len1 age5 2002  240
+    "), "raw_report__num: Can see growth happening")
+
+    ok(cmp_array(r$agg_report__num, "
+        length   age time Freq
+          len1 young 2000  120
+          len1   old 2000  180
+          len1 young 2001   60
+          len1   old 2001  240
+          len1 young 2002   20
+          len1   old 2002  280
+    "), "agg_report__num: Aggregated report only has young/old")
+
+    tmb_r_compare(model_fn, model_tmb, params)
+})
