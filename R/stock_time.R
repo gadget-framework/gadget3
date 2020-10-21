e4bc43b080dee7fa4a5b236624da42748b15427e
Jamie Lentin <jm@lentin.co.uk>
2020-10-21 09:26:51 +0100

    stock_time: Pass through $iterate
    Whilst it doesn't make sense, we need it for stock_reshape to do it's
    thing.

--- R/stock_time.R
+++ R/stock_time.R
@@ -35,7 +35,7 @@ g3s_time <- function(inner_stock, times, year = NULL, step = NULL) {
             time = length(times)),
         dimnames = c(inner_stock$dimnames, list(
             time = times)),
-        iterate = ~stop("Not implemented"),
+        iterate = inner_stock$iterate,  # NB: Just pass through, iterating over all time doesn't make sense
         iter_ss = as.call(c(as.list(inner_stock$iter_ss), as.symbol("stock__time_idx"))),
         intersect = f_substitute(~g3_with(stock__time_idx, g3_idx(idx_f), if (stock__time_idx >= g3_idx(1)) extension_point), list(
                 idx_f = idx_f,
