942e541874e54693ae0bc0006eba9cc8e4cf33c3
Jamie Lentin <jm@lentin.co.uk>
2020-11-08 17:20:16 +0000

    action_naturalmortality: Multiple g3a_naturalmortality actions

--- R/action_naturalmortality.R
+++ R/action_naturalmortality.R
@@ -7,11 +7,12 @@ g3a_naturalmortality_exp <- function (param_f) {
 g3a_naturalmortality <- function (stock, mortality_f, run_f = TRUE, run_at = 4) {
     # See Stock::reducePop, NaturalMortality::Reset
     out <- new.env(parent = emptyenv())
+    action_name <- unique_action_name()
 
     stock__num <- stock_instance(stock)
     stock__wgt <- stock_instance(stock)
 
-    out[[step_id(run_at, stock)]] <- g3_step(f_substitute(~if (run_f) {
+    out[[step_id(run_at, stock, action_name)]] <- g3_step(f_substitute(~if (run_f) {
         debug_label("Natural mortality for ", stock)
         stock_iterate(stock, {
             stock_ss(stock__num) <- stock_ss(stock__num) * (mortality_f)
