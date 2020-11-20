9cf2f9916bb47ad547a55089f6448683f213accd
Jamie Lentin <jm@lentin.co.uk>
2020-11-20 16:51:54 +0000

    stock: Break down stock into g3_storage and g3s_length
    Bit odd currently, but makes more sense long-term.

--- R/stock.R
+++ R/stock.R
@@ -13,7 +13,18 @@ stock_instance <- function (stock, init_value = NA) {
     return(array(init_value, dim = stock$dim, dimnames = stock$dimnames))
 }
 
-g3_stock <- function(var_name, lengthgroups, open_ended = TRUE) {
+g3_storage <- function(var_name) {
+    structure(list(
+        dim = list(),
+        dimnames = list(),
+        iterate = ~extension_point,
+        iter_ss = list(),  # NB: No dimensions yet
+        intersect = ~extension_point,
+        rename = ~extension_point,
+        name = var_name), class = c("g3_stock", "list"))
+}
+
+g3s_length <- function(inner_stock, lengthgroups, open_ended = TRUE) {
     # See LengthGroupDivision::LengthGroupDivision
     stopifnot(length(lengthgroups) > 0)
 
@@ -41,27 +52,23 @@ g3_stock <- function(var_name, lengthgroups, open_ended = TRUE) {
     stock__midlen <- as.array(lengthgroups + (stock__dl / 2))
 
     structure(list(
-        dim = c(
+        dim = c(inner_stock$dim,
             length = length(lengthgroups)),
-        dimnames = list(
-            length = names(lengthgroups)),
-        iterate = ~extension_point,
-        iter_ss = list(quote(.[])[[3]]),  # NB: This includes a missing parameter for the length dimension
-        intersect = ~extension_point,
-        rename = ~extension_point,
-        name = var_name), class = c("g3_stock", "list"))
+        dimnames = c(inner_stock$dimnames, list(
+            length = names(lengthgroups))),
+        iterate = f_substitute(~extension_point, list(
+            extension_point = inner_stock$iterate)),
+        # NB: Length iterator is always "missing", so we get a lengthgroup vector
+        iter_ss = c(inner_stock$iter_ss, list(quote(.[])[[3]])),
+        intersect = f_substitute(~extension_point, list(
+            extension_point = inner_stock$intersect)),
+        rename = f_substitute(~extension_point, list(
+            extension_point = inner_stock$rename)),
+        name = inner_stock$name), class = c("g3_stock", "list"))
 }
 
-g3_fleet <- function(var_name) {
-    structure(list(
-        dim = c(),
-        dimnames = list(),
-        iterate = ~extension_point,
-        iter_ss = list(),  # NB: No dimensions yet
-        intersect = ~extension_point,
-        rename = ~extension_point,
-        name = var_name), class = c("g3_stock", "list"))
-}
+g3_fleet <- function(var_name) g3_storage(var_name)
+g3_stock <- function(var_name, lengthgroups, open_ended = TRUE) g3s_length(g3_storage(var_name), lengthgroups, open_ended)
 
 g3s_clone <- function(inner_stock, var_name) {
     new_stock <- inner_stock
