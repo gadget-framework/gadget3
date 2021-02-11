b7a616da9e449684a9ff555be568242794949049
Jamie Lentin <jm@lentin.co.uk>
2021-02-11 10:17:20 +0000

    run_tmb: More compiler flags for windows #12
    Compiler bugs(?) are resulting in the following:
    
        Error: invalid register for .seh_savexmm
    
    Disable more optimisations to try and work around

--- R/run_tmb.R
+++ R/run_tmb.R
@@ -790,7 +790,11 @@ print.g3_cpp <- function(x, ...) {
 # Turn a g3 TMB bit of code into an adfun
 g3_tmb_adfun <- function(cpp_code,
                          parameters = attr(cpp_code, 'parameter_template'),
-                         compile_flags = if (.Platform$OS.type == "windows") c("-O3", "-march=native") else c("-O3", "-flto", "-march=native"),
+                         compile_flags =
+                             # No -flto since it won't(?) work out of the box
+                             # https://stackoverflow.com/questions/43152633/invalid-register-for-seh-savexmm-in-cygwin
+                             if (.Platform$OS.type == "windows") c("-O3", "-march=native", "-fno-asynchronous-unwind-tables")
+                             else c("-O3", "-flto", "-march=native"),
                          work_dir = tempdir(),
                          output_script = FALSE, ...) {
     model_params <- attr(cpp_code, 'parameter_template')
