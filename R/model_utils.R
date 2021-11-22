4791b428ef8768e1029b10c4d1cee4e21c366bca
will <will.butler@hafogvatn.is>
2021-11-22 14:44:57 +0000

    model_utils: initial vonb 'K' enclosed

--- R/model_utils.R
+++ R/model_utils.R
@@ -19,7 +19,7 @@ g3a_initial_abund <- function(scalar,
 
 g3a_initial_vonb <- function(Linf, K, recl, K_scale = 0.001){
   f_substitute(
-    ~Linf * (1 - exp(-1 * K_scale * K * (age - (1 + log(1 - recl/Linf)/(K_scale*K))))),
+    ~Linf * (1 - exp(-1 * (K_scale * K) * (age - (1 + log(1 - recl/Linf)/(K_scale*K))))),
     list(K_scale = K_scale,
          Linf = Linf,
          K = K,
