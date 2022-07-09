20ebf60675c198f44388a6bd369894acc7c606ad
Jamie Lentin <jm@lentin.co.uk>
2022-07-09 11:45:24 +0100

    params: Add g3_parameterized # 58
    A new "do-everything" method acting as a front-end to g3_param,
    stock_param, etc.
    
    Covers the cases that gadgetutils::g3_stock_* methods handled, so
    calls to those could be replaced with this easily.

---
+++ R/params.R
@@ -0,0 +1,78 @@
+g3_parameterized <- function(
+        name,
+        by_stock = FALSE,
+        by_year = FALSE,
+        by_age = FALSE,
+        exponentiate = FALSE,
+        ...) {
+    stopifnot(is.character(name))
+    stopifnot(is.logical(by_age))
+    stopifnot(is.logical(by_year))
+
+    table_defn <- list()
+    if (by_year) {
+        table_defn <- c(table_defn, list( cur_year = quote( seq(start_year, end_year) ) ))
+    }
+
+    if (isFALSE(by_stock)) {  # No grouping by stock
+        if (by_age) stop("!by_stock && by_age doesn't make sense")
+
+        if (length(table_defn) > 0) {
+            table_defn <- as.call(c(as.symbol('expand.grid'), table_defn))
+            out <- substitute(g3_param_table(x, table_defn), list(x = name, table_defn = table_defn))
+        } else {
+            out <- substitute(g3_param(x), list(x = name))
+        }
+    } else if (isTRUE(by_stock) || is.character(by_stock)) {  # Group by default "stock", with an optional name_part
+        if (by_age) {
+            table_defn <- c(table_defn, list(age = quote(seq(stock__minage, stock__maxage))))
+        }
+
+        # Use stock_param() to do the substitutions later
+        name_part <- if (isTRUE(by_stock)) NULL else by_stock
+        if (length(table_defn) > 0) {
+            table_defn <- as.call(c(as.symbol('expand.grid'), table_defn))
+            out <- substitute(stock_param_table(stock, x, name_part = name_part, table_defn), list(x = name, name_part = name_part, table_defn = table_defn))
+        } else {
+            out <- substitute(stock_param(stock, x, name_part = name_part), list(x = name, name_part = name_part))
+        }
+    } else if (g3_is_stock(by_stock) || (is.list(by_stock) && all(sapply(by_stock, g3_is_stock)))) {  # Group by explicit stocks
+        if (g3_is_stock(by_stock)) by_stock <- list(by_stock)
+        names(by_stock) <- vapply(by_stock, function(s) s$name, character(1))
+
+        # by_age is union of all stock ages
+        # NB: We're not, ensuring s__minage are available. This is a bit naughty, but it's very improbable it's a problem
+        if (by_age) {
+            for (s in by_stock) {
+                if (!("age" %in% names(s$dim))) stop(s$name, " has no age dimension, by_age = TRUE doesn't make sense")
+            }
+            min_age <- as.call(c(as.symbol('min'), lapply(
+                unname(by_stock),
+                function (s) as.symbol(paste0(s$name, '__minage')))))
+            max_age <- as.call(c(as.symbol('max'), lapply(
+                unname(by_stock),
+                function (s) as.symbol(paste0(s$name, '__maxage')))))
+            table_defn <- c(table_defn, list(age = substitute(seq(min_age, max_age), list(min_age = min_age, max_age = max_age))))
+        }
+
+        # Find common name_part, add that to our name
+        common_part <- rep(TRUE, length(by_stock[[1]]$name_parts))
+        for (i in seq_along(by_stock)) if (i > 1) {
+            common_part <- common_part & (by_stock[[1]]$name_parts == by_stock[[i]]$name_parts)
+        }
+        name <- paste(c(by_stock[[1]]$name_parts[common_part], name), collapse = ".")
+        
+        # Generate g3_param call (NB: not stock_param, we've done the renaming ourselves)
+        if (length(table_defn) > 0) {
+            table_defn <- as.call(c(as.symbol('expand.grid'), table_defn))
+            out <- substitute(g3_param_table(x, table_defn), list(x = name, table_defn = table_defn))
+        } else {
+            out <- substitute(g3_param(x), list(x = name))
+        }
+    } else stop('Unknown by_stock parameter, should be FALSE, TRUE, a name_part or list of stocks')
+
+    # Pass through standard g3_param arguments
+    out <- as.call(c(as.list(out), list(...)))
+    if (exponentiate) out <- substitute(exp(x), list(x = out))
+    return(out)
+}
---
+++ tests/test-params.R
@@ -0,0 +1,76 @@
+library(unittest)
+if (!interactive()) options(warn=2, error = function() { sink(stderr()) ; traceback(3) ; q(status = 1) })
+
+library(gadget3)
+
+cmp_code <- function (a, b) ut_cmp_identical(deparse(a), deparse(b))
+
+stock_mimm <- g3_stock(c(species = 'st', sex = 'm', maturity = 'imm'), seq(10, 35, 5)) |> g3s_age(1, 5)
+stock_mmat <- g3_stock(c(species = 'st', sex = 'm', maturity = 'mat'), seq(10, 35, 5)) |> g3s_age(3, 7)
+stock_fimm <- g3_stock(c(species = 'st', sex = 'f', maturity = 'imm'), seq(10, 35, 5)) |> g3s_age(1, 5)
+stock_fmat <- g3_stock(c(species = 'st', sex = 'f', maturity = 'mat'), seq(10, 35, 5)) |> g3s_age(3, 7)
+# Put x through g3_step() as a g3 action would, the "stock" being stock_mimm
+pretend_stock_action <- function (x) {
+    rlang::f_rhs(gadget3:::g3_step(gadget3:::call_to_formula(
+        x,
+        env = as.environment(list(stock = stock_mimm)))))
+}
+
+ok(cmp_code(
+    call("{",  # }
+        g3_parameterized('parp', by_stock = FALSE),
+    NULL), quote({
+        g3_param("parp")
+    NULL})), "Not 'by' anything, so just a regular parameter")
+
+ok(cmp_code(
+    call("{",  # }
+        g3_parameterized('parp', by_stock = FALSE, value = 4, lower = 2, upper = 9),
+        g3_parameterized('parp', by_stock = FALSE, optimise = FALSE),
+        g3_parameterized('parp', by_stock = FALSE, random = TRUE),
+    NULL), quote({
+        g3_param("parp", value = 4, lower = 2, upper = 9)
+        g3_param("parp", optimise = FALSE)
+        g3_param("parp", random = TRUE)
+    NULL})), "Extra parameters passed through")
+
+ok(cmp_code(
+    pretend_stock_action(g3_parameterized('parp', by_stock = TRUE)),
+    quote(g3_param("st_m_imm.parp"))), "by_stock, so will use stock_param() to rename variables")
+
+ok(ut_cmp_error(
+    g3_parameterized('parp', by_stock = FALSE, by_age = TRUE),
+    'by_age'), "!by_stock and by_age is nonsensical, throws error")
+
+ok(cmp_code(
+    pretend_stock_action(call("{",  # }
+        g3_parameterized('parp', by_stock = TRUE, by_year = TRUE),
+        g3_parameterized('parp', by_stock = TRUE, by_year = TRUE, by_age = TRUE),
+    NULL)), quote({
+        g3_param_table("st_m_imm.parp", expand.grid(
+            cur_year = seq(start_year, end_year)))
+        g3_param_table("st_m_imm.parp", expand.grid(
+            cur_year = seq(start_year, end_year),
+            age = seq(st_m_imm__minage, st_m_imm__maxage)))
+    NULL})), "Adding by_year or by_age turns it into a table")
+
+ok(cmp_code(
+    pretend_stock_action(call("{",  # }
+        g3_parameterized('parp', by_stock = 'species', lower = 3),
+        g3_parameterized('parp', by_stock = 'sex', by_year = TRUE),
+    NULL)), quote({
+        g3_param("st.parp", lower = 3)
+        g3_param_table("m.parp", expand.grid(cur_year = seq(start_year, end_year)))
+    NULL})), "Can specify which name_part should be used in the name")
+
+ok(cmp_code(
+    call("{",  # }
+        g3_parameterized('parp', by_stock = list(stock_mimm, stock_mmat)),
+        g3_parameterized('parp', by_stock = list(stock_mimm, stock_fmat)),  # M vs F, so only species matches
+        g3_parameterized('parp', by_stock = list(stock_fimm, stock_fmat), by_age = TRUE),
+    NULL), quote({
+        g3_param("st.m.parp")
+        g3_param("st.parp")
+        g3_param_table("st.f.parp", expand.grid(
+            age = seq(min(st_f_imm__minage, st_f_mat__minage), max(st_f_imm__maxage, st_f_mat__maxage))))
+    NULL})), "Can give a list of stocks, in which case it works out name parts for you")
