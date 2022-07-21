c132d91f7f61e289e7426281aba821358be733a5
Jamie Lentin <jm@lentin.co.uk>
2022-07-21 17:37:27 +0100

    inttest/renewal: Fix g2/g3 model differences
    * Stock identifiers should match
    * normalparam files that rgadget can read
    * Formulas that match the g2 equivalents

--- inttest/renewal/run.R
+++ inttest/renewal/run.R
@@ -6,7 +6,7 @@ library(unittest)
 
 year_range <- 1982:1990
 
-ling_imm <- g3_stock('ling_imm', seq(20, 156, 4)) %>% 
+lingimm <- g3_stock('lingimm', seq(20, 156, 4)) %>% 
     g3s_livesonareas(c(1)) %>%
     g3s_age(3, 5)
 
@@ -17,65 +17,65 @@ imm_report <- g3_stock('imm_report', seq(20, 160, 4), open_ended = FALSE) %>%
   g3s_age(3, 5) %>%
   g3s_time(year = local(year_range), step = 1:4)
 
-ling_imm_actions <- list(
-    g3a_initialconditions_normalparam(ling_imm,
+lingimm_actions <- list(
+    g3a_initialconditions_normalparam(lingimm,
         factor_f = ~g3_param("lingimm.init") * g3_param("lingimm.init.scalar"),
         mean_f = ~g3_param("ling.Linf"),
         stddev_f = ~10, 
         alpha_f = ~g3_param("lingimm.walpha"),
         beta_f = ~g3_param("lingimm.wbeta")),
-    g3a_renewal_normalparam(ling_imm,
+    g3a_renewal_normalparam(lingimm,
         factor_f = ~g3_param("lingimm.rec.scalar"),
         mean_f = ~g3_param("ling.Linf"),
         stddev_f = ~10, 
         alpha_f = ~g3_param("lingimm.walpha"),
         beta_f = ~g3_param("lingimm.wbeta"),
         run_f = ~cur_step == 1 && age == 3),
-    g3a_naturalmortality(ling_imm, g3a_naturalmortality_exp(~g3_param("lingimm.M"))),
-    g3a_growmature(ling_imm,
+    g3a_naturalmortality(lingimm, g3a_naturalmortality_exp(~g3_param("lingimm.M"))),
+    g3a_growmature(lingimm,
         impl_f = g3a_grow_impl_bbinom(
-            g3a_grow_lengthvbsimple(~g3_param("ling.Linf"), ~g3_param("ling.k") * 0.001),
+            g3a_grow_lengthvbsimple(~g3_param("ling.Linf"), ~0.001 * g3_param("ling.k")),
             g3a_grow_weightsimple(~g3_param("lingimm.walpha"), ~g3_param("lingimm.wbeta")),
-            beta_f = ~g3_param("ling.bbin") * 10,
+            beta_f = ~10 * g3_param("ling.bbin"),
             maxlengthgroupgrowth = 15)),
-    g3a_age(ling_imm),
+    g3a_age(lingimm),
     list())
 
 fleet_actions <- list(
-    g3a_predate_fleet(igfs, list(ling_imm),
+    g3a_predate_fleet(igfs, list(lingimm),
         suitabilities = list(
-            ling_imm = g3_suitability_exponentiall50(~g3_param('ling.igfs.alpha'), ~g3_param('ling.igfs.l50'))),
+            lingimm = g3_suitability_exponentiall50(~g3_param('ling.igfs.alpha'), ~g3_param('ling.igfs.l50'))),
         catchability_f = g3a_predate_catchability_totalfleet(g3_timeareadata('igfs_landings', Rgadget::read.gadget.file('inttest/renewal/', 'Data/fleet.igfs.data')[[1]], 'number'))),
     list())
 
 ling_likelihood_actions <- list(
     g3l_understocking(
         weight = 100,
-        list(ling_imm)),
+        list(lingimm)),
     g3l_catchdistribution(
-        'ldist_igfs',
+        'ldist.igfs',
         weight = 1,
         obs_data = structure(
             Rgadget::read.gadget.file('inttest/renewal/', 'Data/catchdistribution.ldist.igfs.sumofsquares')[[1]],
             age = list(all3 = 3:5),
             length = Rgadget::read.gadget.file('inttest/renewal','Aggfiles/catchdistribution.ldist.igfs.len.agg')[[1]]),
         fleets = list(igfs),
-        stocks = list(ling_imm),
+        stocks = list(lingimm),
         g3l_distribution_sumofsquares()),
     list())
 
 report_actions <- list(
-       g3a_report_stock(imm_report,ling_imm, ~stock_ss(ling_imm__num)),
-       g3a_report_stock(imm_report,ling_imm, ~stock_ss(ling_imm__wgt)),
-#       g3a_report_stock(imm_report,ling_imm, ~stock_ss(ling_imm__igfs)),
+       g3a_report_stock(imm_report,lingimm, ~stock_ss(lingimm__num)),
+       g3a_report_stock(imm_report,lingimm, ~stock_ss(lingimm__wgt)),
+#       g3a_report_stock(imm_report,lingimm, ~stock_ss(lingimm__igfs)),
        list())
 
 time_actions <- list(
-    g3a_time(min(year_range), max(year_range), c(3,3,3,3), project_years = 0),
+    g3a_time(min(year_range), max(year_range), c(3,3,3,3)),
     list())
 
 actions <- c(
-  ling_imm_actions,
+  lingimm_actions,
 #  fleet_actions,
 #  ling_likelihood_actions,
   report_actions,
