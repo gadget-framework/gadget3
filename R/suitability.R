b5f4ec4f55de907180398e4ac7016321bbb6bede
Bjarki Þór Elvarsson <bjarki.elvarsson@hafogvatn.is>
2022-10-03 10:15:59 +0000

    andersen suitability parameters are not allowed to be negative.

--- R/suitability.R
+++ R/suitability.R
@@ -11,9 +11,9 @@ g3_suitability_exponentiall50 <- function (alpha, l50) {
 g3_suitability_andersen <- function (p0, p1, p2, p3 = p4, p4, p5 = ~pred_stock__midlen) {
   # TODO: We need to switch p4 for p3 when log(pred_stock__midlen/p5) - p1 is <= 0
  # if (!identical(p3, p4)) stop("p3 not currently supported")
-  f_substitute(~p0 + 
-                 p2 * exp(-(log(p5/stock__midlen) - p1)**2/p4) * bounded_vec(100*(p1 - log(p5/stock__midlen)),0,1) +
-                 p2 * exp(-(log(p5/stock__midlen) - p1)**2/p3) * bounded_vec(100*(log(p5/stock__midlen)) - p1,0,1),
+  f_substitute(~avoid_zero(p0) + 
+                 avoid(p2) * exp(-(log(avoid_zero(p5/stock__midlen)) - p1)**2/avoid_zero(p4)) * bounded_vec(100*(p1 - log(avoid_zero(p5/stock__midlen))),0,1) +
+                 p2 * exp(-(log(avoid_zero(p5/stock__midlen)) - p1)**2/avoid_zero(p3)) * bounded_vec(100*(log(avoid_zero(p5/stock__midlen))) - p1,0,1),
                list(
                  p0 = p0,
                  p1 = p1,
