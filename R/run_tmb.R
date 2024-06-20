a5d778a47e5246ec32eb484f88db54c9e512dd40
Jamie Lentin <jm@lentin.co.uk>
2024-06-20 16:32:02 +0000

    run_tmb: Normalise argument spacing
    Use same argument spacing R<->Rd

--- R/run_tmb.R
+++ R/run_tmb.R
@@ -990,16 +990,18 @@ print.g3_cpp <- function(x, ...) {
 }
 
 # Turn a g3 TMB bit of code into an adfun
-g3_tmb_adfun <- function(cpp_code,
-                         parameters = attr(cpp_code, 'parameter_template'),
-                         compile_flags =
-                             # No -flto since it won't(?) work out of the box
-                             # https://stackoverflow.com/questions/43152633/invalid-register-for-seh-savexmm-in-cygwin
-                             # -O1, since -O3 is causing unscruitable problems linking as C++
-                             if (.Platform$OS.type == "windows") c("-O1", "-march=native")
-                             else c("-O3", "-flto", "-march=native"),
-                         work_dir = tempdir(),
-                         output_script = FALSE, ...) {
+# NB: No -flto for windows since it won't(?) work out of the box
+#     https://stackoverflow.com/questions/43152633/invalid-register-for-seh-savexmm-in-cygwin
+#     -O1, since -O3 is causing unscruitable problems linking as C++
+g3_tmb_adfun <- function(
+        cpp_code,
+        parameters = attr(cpp_code, 'parameter_template'),
+        compile_flags =
+            if (.Platform$OS.type == "windows") c("-O1", "-march=native")
+            else c("-O3", "-flto", "-march=native"),
+        work_dir = tempdir(),
+        output_script = FALSE,
+        ...) {
     model_params <- attr(cpp_code, 'parameter_template')
 
     # If parameters is a list, merge into our data.frames
