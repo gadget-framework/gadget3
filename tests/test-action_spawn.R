bbc44b84e646e686997f2bb31a1d517dce10287d
Jamie Lentin <jm@lentin.co.uk>
2024-07-17 11:16:59 +0000

    test-action_spawn: Revamp recruitment tests
    Test each recruitment function by putting through random values and
    independently verifying the results.

--- tests/test-action_spawn.R
+++ tests/test-action_spawn.R
@@ -1,3 +1,4 @@
+if (!interactive()) options(warn=2, error = function() { sink(stderr()) ; traceback(3) ; q(status = 1) })
 library(magrittr)
 library(unittest)
 
@@ -35,117 +36,61 @@ ok(ut_cmp_error(
 ok_group('g3a_spawn', {
     year_range <- 1982:1990
 
-    ling_imm <- g3_stock('ling_imm', seq(20, 156, 4)) %>%
-        g3s_livesonareas(c(1)) %>%
-        g3s_age(3, 10)
-
-    ling_mat <- g3_stock('ling_mat', seq(20, 156, 4)) %>%
-        g3s_livesonareas(c(1)) %>%
-        g3s_age(5, 15)
-
-    igfs <- g3_fleet('igfs') %>% g3s_livesonareas(c(1))
-
-    imm_report <- g3s_clone(ling_imm, 'imm_report') %>% g3s_time(year = local(year_range), step = 1:4)
-    mat_report <- g3s_clone(ling_mat, 'mat_report') %>% g3s_time(year = local(year_range), step = 1:4)
-
-    ling_imm_actions <- list(
-        g3a_initialconditions_normalparam(ling_imm,
-            factor_f = ~age * g3_param("lingimm.init") * g3_param("lingimm.init.scalar"),
-            mean_f = ~g3_param("ling.Linf"),
-            stddev_f = ~10,
-            alpha_f = ~g3_param("lingimm.walpha"),
-            beta_f = ~g3_param("lingimm.wbeta")),
-        g3a_naturalmortality(ling_imm, g3a_naturalmortality_exp(~g3_param("lingimm.M"))),
-        g3a_age(ling_imm),
-        list())
-
-    ling_mat_actions <- list(
-        g3a_initialconditions_normalparam(ling_mat,
-            factor_f = ~age * g3_param("lingmat.init") * g3_param("lingmat.init.scalar"),
-            mean_f = ~g3_param("ling.Linf"),
-            stddev_f = ~10,
-            alpha_f = ~g3_param("lingmat.walpha"),
-            beta_f = ~g3_param("lingmat.wbeta")),
-        g3a_naturalmortality(ling_mat, g3a_naturalmortality_exp(~g3_param("lingmat.M"))),
-        g3a_age(ling_mat),
-        g3a_spawn(
-            ling_mat,
-            recruitment_f = g3a_spawn_recruitment_ricker(
-                ~g3_param("ricker.mu"),
-                ~g3_param("ricker.lambda")),
-            proportion_f = g3_suitability_exponentiall50(alpha = ~-g3_param("spawn.prop.alpha"), l50 =  ~g3_param("spawn.prop.l50")),
-            mortality_f = g3_suitability_straightline(alpha = ~g3_param("spawn.mort.alpha"), beta =  ~g3_param("spawn.mort.beta")),
-            weightloss_f = ~g3_param("spawn.weightloss"),
-            output_stocks = list(ling_imm),
-            mean_f = 50,
-            stddev_f = 0.9,
-            alpha_f = 1,
-            beta_f = 1,
-            run_step = 1 ),
-        list())
-
-    report_actions <- list(
-           g3a_report_stock(imm_report,ling_imm, ~stock_ss(ling_imm__num)),
-           g3a_report_stock(imm_report,ling_imm, ~stock_ss(ling_imm__wgt)),
-           g3a_report_stock(mat_report,ling_mat, ~stock_ss(ling_mat__num)),
-           g3a_report_stock(mat_report,ling_mat, ~stock_ss(ling_mat__wgt)),
-           list())
-
-    time_actions <- list(
-        g3a_time(min(year_range), max(year_range), c(3,3,3,3), project_years = 0),
-        list())
-
-    # Add steps to exercise rest of recruitment functions, and check they produce identical TMB results
+    # Add spawn step, spawning into a new stock with a name matching the recruitment function
     recruitment_test_step <- function (recruitment_f) {
-        action_name <- gadget3:::unique_action_name()
+        fn_name <- as.character(sys.call()[[2]][[1]])
+        mat_stock <- g3_stock(c("mat", fn_name), seq(10, 40, 10)) %>% g3s_age(5, 5)
+        imm_stock <- g3_stock(c("imm", fn_name), seq(10, 40, 10)) %>% g3s_age(0, 0)
 
-        # Re-implement enough of spawning to test recruitment
-        stock <- ling_mat
-        out_var_name <- paste0('stock__rf_', sys.call()[[2]][[1]])
-        assign(out_var_name, g3_stock_instance(stock))
-        out <- list()
-        out[[gadget3:::step_id(999, action_name)]] <- gadget3:::g3_step(gadget3:::f_substitute(
-            ~g3_with(s := 0 * nll, {  # TODO: Ugly mess to get type right
-                stock_iterate(stock, if (run_f) {
-                    s <- s + recruitment_s_f
-                    stock_ss(stock__outvar) <- 1
-                } else {
-                    stock_ss(stock__outvar) <- 0
-                })
-                g3_with(r := recruitment_r_f,
-                    stock_with(stock, stock__outvar <- r * stock__outvar / avoid_zero(sum(stock__outvar))))
-            }),
-            list(
-                recruitment_r_f = recruitment_f$r,
-                recruitment_s_f = recruitment_f$s,
-                stock__outvar = as.symbol(out_var_name))))
-        return(out)
+        return(list(
+            g3a_otherfood(  # Matures steadily grow over model run
+                mat_stock,
+                num_f = g3_formula(
+                    cur_time * s1 + stock__midlen * 0,
+                    s1 = g3_parameterized("abund_scalar", value = runif(1, min=90, max=110)) ),
+                wgt_f = g3_formula(
+                    cur_time * s2 + stock__midlen * 0,
+                    s2 = g3_parameterized("mwgt_scalar", value = runif(1, min=9, max=11)) )),
+            g3a_otherfood(  # Empty immature after each step
+                imm_stock,
+                num_f = quote( stock__midlen * 0 ),
+                wgt_f = 1 ),
+            g3a_spawn(
+                mat_stock,
+                recruitment_f,
+                output_stocks = list(imm_stock) )))
     }
-    recruitment_f_actions <- list(
+    actions <- list(
+        g3a_time(min(year_range), max(year_range), c(3,3,3,3), project_years = 0),
         recruitment_test_step(g3a_spawn_recruitment_fecundity(
-            p0 = runif(1, min=0.1, max=0.9),
-            p1 = runif(1, min=0.1, max=0.9),
-            p2 = runif(1, min=0.1, max=0.9),
-            p3 = runif(1, min=0.1, max=0.9),
-            p4 = runif(1, min=0.1, max=0.9))),
-        recruitment_test_step(g3a_spawn_recruitment_simplessb(runif(1, min=0.1, max=0.9))),
-        recruitment_test_step(g3a_spawn_recruitment_ricker(runif(1, min=0.1, max=0.9), runif(1, min=0.1, max=0.9))),
-        recruitment_test_step(g3a_spawn_recruitment_bevertonholt(runif(1, min=0.1, max=0.9), runif(1, min=0.1, max=0.9))),
-        recruitment_test_step(g3a_spawn_recruitment_hockeystick(runif(1, min=0.1, max=0.9), runif(1, min=0.1, max=0.9))),
+            g3_parameterized("fecundity_p0", value = runif(1, min=0.1, max=0.9)),
+            g3_parameterized("fecundity_p1", value = runif(1, min=0.1, max=0.9)),
+            g3_parameterized("fecundity_p2", value = runif(1, min=0.1, max=0.9)),
+            g3_parameterized("fecundity_p3", value = runif(1, min=0.1, max=0.9)),
+            g3_parameterized("fecundity_p4", value = runif(1, min=0.1, max=0.9)) )),
+        recruitment_test_step(g3a_spawn_recruitment_simplessb(
+            g3_parameterized("simplessb_mu", value = runif(1, min=0.1, max=0.9)) )),
+        recruitment_test_step(g3a_spawn_recruitment_ricker(
+            g3_parameterized("ricker_mu", value = runif(1, min=0.9, max=1.2)),
+            g3_parameterized("ricker_lambda", value = runif(1, min=1e-6, max=1e-5)) )),
+        recruitment_test_step(g3a_spawn_recruitment_bevertonholt(
+            g3_parameterized("bevertonholt_mu", value = runif(1, min=0.4, max=0.6)),
+            g3_parameterized("bevertonholt_lambda", value = runif(1, min=0.4, max=0.6)) )),
+        recruitment_test_step(g3a_spawn_recruitment_hockeystick(
+            g3_parameterized("hockeystick_r0", value = runif(1, min=0.4, max=0.6)),
+            g3_parameterized("hockeystick_blim", value = runif(1, min=0.4, max=0.6)) )),
         list())
 
-    actions <- c(
-      ling_imm_actions,
-      ling_mat_actions,
-      report_actions,
-      recruitment_f_actions,
-      time_actions)
+    all_actions <- c(actions, list(
+        g3a_report_history(actions, '__num$|__wgt$|__offspringnum$'),
+        g3a_report_history(actions, '__midlen$', out_prefix = NULL),
+        NULL ))
 
     # Compile model
-    model_fn <- g3_to_r(actions, trace = FALSE)
+    model_fn <- g3_to_r(all_actions, trace = FALSE)
     # model_fn <- edit(model_fn)
     if (nzchar(Sys.getenv('G3_TEST_TMB'))) {
-        model_cpp <- g3_to_tmb(actions, trace = FALSE)
+        model_cpp <- g3_to_tmb(all_actions, trace = FALSE)
         # model_cpp <- edit(model_cpp)
         model_tmb <- g3_tmb_adfun(model_cpp, compile_flags = c("-O0", "-g"))
     } else {
@@ -153,33 +98,64 @@ ok_group('g3a_spawn', {
     }
 
     params <- attr(model_fn, 'parameter_template')
-    params$lingimm.init <- 0
-    params$lingimm.init.scalar <- 0
-    params$lingimm.rec.scalar <- 100
-    params$lingimm.M <- 0
-    params$lingimm.walpha <- 1e-1
-    params$lingimm.wbeta <- 2
-    params$lingmat.init <- 1
-    params$lingmat.init.scalar <- 100
-    params$lingmat.rec.scalar <- 100
-    params$lingmat.M <- 0.95
-    params$lingmat.walpha <- 1e-6
-    params$lingmat.wbeta <- 2
-    params$ling.init.F <- 0.4
-    params$ling.mat.alpha <- 0.01
-    params$ling.mat.l50 <- 75
-    params$ling.mat.beta <- 0.01
-    params$ling.mat.a50 <- 7
-    params$ling.Linf <- 160
-    params$ling.bbin <- 6
-    params$ling.k <- 10
-    params$ricker.mu <- 1
-    params$ricker.lambda <- 1e-6
-    params$spawn.prop.alpha <- 0.5
-    params$spawn.prop.l50 <- 120
-    params$spawn.mort.alpha <- 0
-    params$spawn.mort.beta <- 0
-    params$spawn.weightloss <- 0.1
+
+    r <- lapply(attributes(model_fn(params)), drop)  # NB: Drop all redundant dimensions,
+    age <- 5
+    # Repeat midlen history for each time bin
+    hist_midlen <- array(r$mat_g3a_spawn_recruitment_fecundity__midlen, dim = c(4, 36))
+    hist_biomass <- colSums(r$hist_mat_g3a_spawn_recruitment_simplessb__num * r$hist_mat_g3a_spawn_recruitment_simplessb__wgt)
+    ok(ut_cmp_equal(
+        as.vector(colSums(r$hist_mat_g3a_spawn_recruitment_simplessb__num)),
+        4 * 0:35 * params$abund_scalar,
+        end = NULL ), "hist_mat_g3a_spawn_recruitment_simplessb__num: Test rig produced increasing abundance" )
+    ok(ut_cmp_equal(
+        as.vector(colSums(r$hist_mat_g3a_spawn_recruitment_simplessb__wgt)),
+        4 * 0:35 * params$mwgt_scalar,
+        end = NULL ), "hist_mat_g3a_spawn_recruitment_simplessb__wgt: Test rig produced increasing mean weight" )
+
+    ok(ut_cmp_equal(
+        as.vector(colSums(r$hist_mat_g3a_spawn_recruitment_fecundity__offspringnum) > 0),
+        c(FALSE, rep(TRUE, 36 - 1)) ), "hist_mat_g3a_spawn_recruitment_fecundity__offspringnum: Some recruitment happened")
+    ok(ut_cmp_equal(
+        colSums(r$hist_mat_g3a_spawn_recruitment_fecundity__offspringnum),
+        colSums(
+            hist_midlen ^ params$fecundity_p1 *
+            age ^ params$fecundity_p2 *
+            r$hist_mat_g3a_spawn_recruitment_simplessb__num^params$fecundity_p3 *
+            r$hist_mat_g3a_spawn_recruitment_simplessb__wgt^params$fecundity_p4 ) * params$fecundity_p0,
+        end = NULL), "hist_mat_g3a_spawn_recruitment_fecundity__offspringnum: Matches formula")
+
+    ok(ut_cmp_equal(
+        as.vector(colSums(r$hist_mat_g3a_spawn_recruitment_simplessb__offspringnum) > 0),
+        c(FALSE, rep(TRUE, 36 - 1)) ), "hist_mat_g3a_spawn_recruitment_simplessb__offspringnum: Some recruitment happened")
+    ok(ut_cmp_equal(
+        colSums(r$hist_mat_g3a_spawn_recruitment_simplessb__offspringnum),
+        hist_biomass * params$simplessb_mu,
+        end = NULL ), "hist_mat_g3a_spawn_recruitment_simplessb__offspringnum: Total biomass * mu")
+
+    ok(ut_cmp_equal(
+        as.vector(colSums(r$hist_mat_g3a_spawn_recruitment_ricker__offspringnum) > 0),
+        c(FALSE, rep(TRUE, 36 - 1)) ), "hist_mat_g3a_spawn_recruitment_ricker__offspringnum: Some recruitment happened")
+    ok(ut_cmp_equal(
+        colSums(r$hist_mat_g3a_spawn_recruitment_ricker__offspringnum),
+        params$ricker_mu * hist_biomass * exp(-params$ricker_lambda * hist_biomass),
+        end = NULL ), "hist_mat_g3a_spawn_recruitment_ricker__offspringnum: Matches formula")
+
+    ok(ut_cmp_equal(
+        as.vector(colSums(r$hist_mat_g3a_spawn_recruitment_bevertonholt__offspringnum) > 0),
+        c(FALSE, rep(TRUE, 36 - 1)) ), "hist_mat_g3a_spawn_recruitment_bevertonholt__offspringnum: Some recruitment happened")
+    ok(ut_cmp_equal(
+        colSums(r$hist_mat_g3a_spawn_recruitment_bevertonholt__offspringnum),
+        params$bevertonholt_mu * hist_biomass/(params$bevertonholt_lambda + hist_biomass),
+        end = NULL ), "hist_mat_g3a_spawn_recruitment_bevertonholt__offspringnum: Matches formula")
+
+    ok(ut_cmp_equal(
+        as.vector(colSums(r$hist_mat_g3a_spawn_recruitment_hockeystick__offspringnum) > 0),
+        c(FALSE, rep(TRUE, 36 - 1)) ), "hist_mat_g3a_spawn_recruitment_hockeystick__offspringnum: Some recruitment happened")
+    ok(ut_cmp_equal(
+        colSums(r$hist_mat_g3a_spawn_recruitment_hockeystick__offspringnum),
+        params$hockeystick_r0 * pmin(hist_biomass/params$hockeystick_blim, 1),
+        end = NULL ), "hist_mat_g3a_spawn_recruitment_hockeystick__offspringnum: Matches formula")
 
     # Make sure the inttest model produces identical output in TMB and R
     if (nzchar(Sys.getenv('G3_TEST_TMB'))) {
