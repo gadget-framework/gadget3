e25936f40707ba93fc147983444adb5ab8caf836
Jamie Lentin <jm@lentin.co.uk>
2024-08-15 08:26:48 +0000

    WIP: likelihood_sparsesample: Sparse-data likelihood component #175
    Instead of building an array of data as with g3l_distribution,
    g3l_sparsesample works with a data.frame of data, representing each
    column as a separate vector.
    
    Incorporate custom measurement features so it can be used to compare
    blubber thickness.

---
+++ R/likelihood_sparsesample.R
@@ -0,0 +1,208 @@
+# df-like storage for table entries
+g3s_sparsedata <- function(var_name, in_df, area_group = NULL) {
+    stopifnot(is.character(var_name))
+
+    iterate <- quote( extension_point )
+    env <- as.environment(list())
+
+    for (n in names(in_df)) {
+        if (n %in% c("year", "step")) next
+
+        if (n == "area") {
+            stop("Area not supported yet")
+        }
+
+        iterate <- substitute(
+            g3_with(n := stock__n[[stock__row_idx]], extension_point), list(
+                n = as.symbol(n),
+                stock__n = as.symbol(paste0("stock__", n)),
+                extension_point = iterate ))
+        env[[paste0("stock__", n)]] <- if (n == "length") in_df$length else as.integer(in_df[[n]])
+    }
+
+    if ("step" %in% names(in_df)) {
+        iterate <- substitute({ if (stock__step[[stock__row_idx]] != cur_step) next; extension_point }, list(extension_point = iterate))
+        env$stock__step <- as.integer(in_df$step)
+    }
+
+    if ("year" %in% names(in_df)) {
+        in_df <- in_df[order(in_df$year),,drop = F]  # Data has to be sorted by year for this to work
+        iterate <- substitute({
+            if (stock__year[[stock__row_idx]] < cur_year) next
+            if (stock__year[[stock__row_idx]] > cur_year) break
+            extension_point
+        }, list(extension_point = iterate))
+        env$stock__year <- as.integer(in_df$year)
+    } else {
+        stop("Data must at least contain a 'year' column")
+    }
+
+    iterate <- substitute( for (stock__row_idx in seq_along(stock__year)) extension_point , list(extension_point = iterate))
+
+    structure(list(
+        dim = list(row = nrow(in_df)),
+        dimnames = list(),
+        iterate = list(row = iterate),
+        iter_ss = list(row = as.symbol("stock__row_idx")),
+        intersect = list(row = quote(stop("stock_intersect not supported for g3_sparsedata"))),
+        interact = list(row = quote(stop("stock_interact not supported for g3_sparsedata"))),
+        with = list(row = quote(extension_point) ),
+        env = env,
+        name_parts = var_name,
+        name = paste(var_name, collapse = "_") ), class = c("g3_sparsedata", "g3_stock", "list"))
+}
+
+g3_sparsedata_instance <- function (stock, init_value = NA, desc = "") {
+    stopifnot(inherits(stock, "g3_sparsedata"))
+
+    if (length(init_value) == 1) {
+        x <- as_force_vector(rep(init_value, stock$dim$row))
+    } else if (length(init_value) == stock$dim$row) {
+        x <- as_force_vector(init_value)
+    } else {
+        stop("Length of init_value ", length(init_value), " doesn't match stock rows ", stock$dim$row)
+    }
+
+    if (nzchar(desc)) {
+        attr(x, 'desc') <- desc
+    }
+    return(x)
+}
+
+g3l_sparsesample_linreg <- function (
+        fit = c('log', 'linear'),
+        slope = 1,
+        intercept = NULL ) {
+    fit <- match.arg(fit)
+
+    N <- if(fit == 'log') ~log(avoid_zero_vec(nllstock__model_sum/nllstock__model_n)) else ~nllstock__model_sum/nllstock__model_n
+    I <- if(fit == 'log') ~log(avoid_zero_vec(nllstock__obs_mean)) else ~nllstock__obs_mean
+
+    nllstock__nll <- g3_global_formula(
+        f_substitute(
+            # NB: If we're not going to compare, don't bother recalculating linear regression
+            ~stock_with(nllstock, if (cur_time != total_steps) nllstock__nll else regression_linear(N, I, intercept_f, slope_f)),
+            list(
+                N = N,
+                I = I,
+                # NB: Can't use NULL in C++, Use NaN instead
+                intercept_f = if (is.null(intercept)) NaN else intercept,
+                slope_f = if (is.null(slope)) NaN else slope)),
+        init_val = c(nll = 0.0, intercept = 0.0, slope = 0.0) )
+    environment(nllstock__nll)$regression_linear <- regression_linear
+    return(~nllstock__nll[[1]])
+}
+
+g3l_sparsesample_sumsquares <- function (
+        weighting = "model_stddev" ) {
+    if (identical(weighting, "model_stddev")) {
+        weighting_f <- quote( 1 / (nllstock__model_sqsum/nllstock__model_n - (nllstock__model_sum/nllstock__model_n)**2) )
+    } else if (identical(weighting, "obs_stddev")) {
+        weighting_f <- quote( 1 / nllstock__obs_stddev**2 )
+    } else {
+        weighting_f <- weighting
+    }
+
+    # Sumofsquares, picking out NaN values (i.e. projections)
+    g3l_sparsesample_sumsquares_stddev <- g3_native(r = function(model_mean, obs_mean, weighting) {
+        finites <- is.finite(model_mean) & is.finite(obs_mean)
+
+        if (any(!is.finite(weighting[finites]))) warning("g3l_sparsesample_sumsquares_stddev: 1/variance is NaN, use a custom weighing")
+        return( weighting[finites] * (model_mean[finites] - obs_mean[finites])^2 )
+    }, cpp = '[&avoid_zero_vec](vector<Type> model_mean, vector<Type> obs_mean, vector<Type> weighting) -> vector<Type> {
+        Eigen::Matrix<bool, Eigen::Dynamic, 1> finites = model_mean.isFinite() && obs_mean.isFinite() && weighting.isFinite();
+
+        if ((!weighting.isFinite()).any()) Rf_warning("g3l_sparsesample_sumsquares_stddev: 1/variance is NaN, use a custom weighing");
+        return(finites.select(weighting, 0) * (finites.select(model_mean, 1) - finites.select(obs_mean, 1)).pow(2) );
+    }', depends = c('avoid_zero_vec'))
+
+    nllstock__nll <- g3_global_formula(f_substitute(
+        ~stock_with(nllstock, if (cur_time != total_steps) nllstock__nll else g3l_sparsesample_sumsquares_stddev(
+            nllstock__model_sum/nllstock__model_n,
+            nllstock__obs_mean,
+            weighting_f * nllstock__obs_n )),
+        # NB: Initialise with a reference to nllstock__model_n, so we copy it's dimensions
+        list(weighting_f = weighting_f) ), init_val = quote(stock_with(nllstock, nllstock__model_n[])) )
+    return(~sum(nllstock__nll))
+}
+
+g3l_sparsesample <- function (
+        nll_name,
+        obs_df,
+        stocks,
+        measurement_f = quote( biomass ),
+        function_f = g3l_sparsesample_linreg(),
+        area_group = NULL,
+        weight = g3_parameterized(paste0(nll_name, "_weight"),
+            optimise = FALSE, value = 1),
+        run_at = g3_action_order$likelihood ) {
+    stopifnot(is.list(stocks) && all(sapply(stocks, g3_is_stock)))
+    out <- new.env(parent = emptyenv())
+
+    # TODO: spabund vs spcatch, not that we have the other mode yet
+    nllstock <- g3s_sparsedata(c("nll", type = "spabund", name = nll_name), obs_df[,-ncol(obs_df), drop = FALSE], area_group = area_group)
+    nllstock__obs_mean <- g3_sparsedata_instance(nllstock, obs_df[, "mean"], desc = paste0(nll_name, " observations"))
+    nllstock__obs_stddev <- if ("stddev" %in% obs_df) g3_sparsedata_instance(nllstock, obs_df[, "stddev"], desc = paste0(nll_name, " observation stddev")) else quote( stop("No observation stddev column") )
+    nllstock__obs_n <- g3_sparsedata_instance(nllstock, if ("number" %in% obs_df) obs_df[, "number"] else 1, desc = paste0(nll_name, " observation number"))
+    nllstock__model_sum <- g3_sparsedata_instance(nllstock, 0, desc = paste0(nll_name, " prediction total"))
+    nllstock__model_sqsum <- g3_sparsedata_instance(nllstock, 0, desc = paste0(nll_name, " prediction squared-sum"))
+    nllstock__model_n <- g3_sparsedata_instance(nllstock, 0, desc = paste0(nll_name, " datapoint count"))
+
+    nllstock__weight <- 0
+
+    g3_step_foreach_stock <- function (stocks, inner, outer_f) {
+        inner_f <- f_concatenate(lapply(stocks, function (stock) {
+            g3_step(inner(stock), recursing = TRUE, orig_env = list2env(list(
+                stock = stock,
+                end = NULL ), parent = environment(outer_f)))
+        }))
+        outer_f <- g3_step(f_substitute(outer_f, list(inner_f = inner_f)))
+        return(outer_f)
+    }
+
+    # We don't mention __obs_mean ourselves, but nllstock__nll will need a definiton
+    local_nll <- function_f
+    environment(environment(local_nll)$nllstock__nll)$nllstock__obs_mean <- nllstock__obs_mean
+    environment(environment(local_nll)$nllstock__nll)$nllstock__obs_n <- nllstock__obs_n
+    environment(environment(local_nll)$nllstock__nll)$nllstock__obs_stddev <- nllstock__obs_stddev
+
+    # For each stock, collect current values for table
+    out[[step_id(run_at, 'g3l_sparsesample', nll_name, 1)]] <- g3_step_foreach_stock(stocks, function (stock) {
+        # Substitute terms in measurement_f
+        # TODO: We should subset what we need, and work with a vector, which means:
+        #       * Getting names(stock$dim) that we are interested in
+        #       * Turning this into a stock_ss(vec = x, x = default) expression
+        #       * Below, measurement / (measurement)^2 should be summed if not single value
+        # TODO: A catch mode, iterating over fleets & using appropriate terms
+        st_measurement_f <- f_substitute(measurement_f, list(
+            wgt = quote( stock_ss(stock__wgt, vec = single) ),
+            # NB: "length" will be the observed length, we should be using the midlen value
+            length = quote( stock__midlen[[stock__length_idx]] ),
+            end = NULL ))
+        # Multiply quantity by number present in this cell
+        st_measurement_f <- f_substitute(
+            quote( stock_ss(stock__num, vec = single) * st_measurement_f ),
+            list( st_measurement_f = st_measurement_f ))
+
+        # NB: Manually g3_with()ing to work around lost stock-variables, should always be inside intersect anyway
+        f_substitute(~stock_intersect(stock, g3_with(measurement := st_measurement_f, {
+            stock_ss(nllstock__model_sum, vec = single) <- stock_ss(nllstock__model_sum, vec = single) + measurement
+            stock_ss(nllstock__model_sqsum, vec = single) <- stock_ss(nllstock__model_sqsum, vec = single) + (measurement)^2
+            stock_ss(nllstock__model_n, vec = single) <- stock_ss(nllstock__model_n, vec = single) + 1
+        })), list(st_measurement_f = st_measurement_f))
+    }, outer_f = ~{
+        debug_label("Gather model predictions matching individuals in ", nllstock, "__obs")
+        stock_iterate(nllstock, inner_f)
+    })
+
+    # Finally, apply function_f to results
+    out[[step_id(run_at, 'g3l_sparsesample', nll_name, 2)]] <- g3_step(f_substitute(~stock_with(nllstock, {
+        debug_label("Add nll for ", nllstock, "__obs vs ", nllstock, "__model")
+        nllstock__weight <- weight
+        nll <- nll + nllstock__weight * local_nll
+        # TODO: Generate stddev ready for reporting?
+    }), list(
+        weight = weight )))
+
+    return(as.list(out))
+}
---
+++ tests/test-likelihood_sparsesample.R
@@ -0,0 +1,129 @@
+if (!interactive()) options(warn=2, error = function() { sink(stderr()) ; traceback(3) ; q(status = 1) })
+library(unittest)
+
+library(gadget3)
+
+st <- g3_stock("stst", c(10, 20, 30)) |> g3s_age(3,5)
+
+obs_linreg_df <- data.frame(
+    # NB: No 1993, just ignored
+    year = rep(c(1990, 1991, 1992, 1994), each = 2),
+    step = 1:2 )
+obs_linreg_df$age <- floor(runif(nrow(obs_linreg_df), min = 3, max = 5.1))
+obs_linreg_df$length <- floor(runif(nrow(obs_linreg_df), min = 10, max = 50))
+obs_linreg_df$mean <- runif(nrow(obs_linreg_df), min = 10, max = 1000)
+
+obs_ss_df <- data.frame(
+    # NB: No step, just year
+    year = c(1990, 1992, 1993) )
+obs_ss_df$mean = runif(nrow(obs_ss_df), min = 10, max = 1000)
+
+actions <- list(
+    g3a_time(1990, 1994, c(6,6)),
+    g3a_otherfood(st,
+        quote( age * 100 + stock__minlen ),
+        quote( cur_year * 1e5 + cur_step * 1e4 + 0 * stock__minlen ) ),
+    g3l_sparsesample(
+        "bt",
+        obs_linreg_df,
+        list(st),
+        measurement_f = g3_formula(
+            wgt + length,
+            end = NULL ),
+        function_f = g3l_sparsesample_linreg(fit = "linear") ),
+    g3l_sparsesample(
+        "cs_model",
+        obs_ss_df,
+        list(st),
+        measurement_f = g3_formula(
+            wgt * age,
+            end = NULL ),
+        function_f = g3l_sparsesample_sumsquares(weighting = 'model_stddev') ),
+    # NB: Dummy parameter so model will compile in TMB
+    ~{nll <- nll + g3_param("x", value = 0)} )
+full_actions <- c(actions, list(
+    g3a_report_history(actions, var_re = "__num$|__wgt$|^nll_spabund_.+__(model|obs)_.+$"),
+    g3a_report_history(actions, var_re = "^nll_spabund_.+__nll$", out_prefix = NULL),
+    NULL))
+model_fn <- g3_to_r(full_actions)
+model_cpp <- g3_to_tmb(full_actions)
+
+attr(model_fn, 'parameter_template') |>
+    identity() -> params
+
+nll <- model_fn(params) ; r <- attributes(nll) ; nll <- as.vector(nll)
+
+# Final result can be derived from stock abundance
+expected_m <- r$hist_stst__num * c(r$hist_stst__wgt + c(15, 25, 35))
+length_grp <- as.character(cut(obs_linreg_df$length, c(9, 20, 30, Inf), right = FALSE, labels = c("10:20", "20:30", "30:Inf")))
+for (i in seq_len(nrow(obs_linreg_df))) {
+    x <- expected_m[
+        length = length_grp[[i]],
+        age = sprintf("age%d", obs_linreg_df[i, "age"]),
+        time = sprintf("%04d-%02d", obs_linreg_df[i, "year"], obs_linreg_df[i, "step"]) ]
+    ok(ut_cmp_equal(
+        as.vector(r$hist_nll_spabund_bt__model_sum[i, '1994-02']),
+        sum(x) ), paste0("r$hist_nll_spabund_bt__model_sum[", i, ", '1994-02']"))
+    ok(ut_cmp_equal(
+        as.vector(r$hist_nll_spabund_bt__model_sqsum[i, '1994-02']),
+        sum(x**2) ), paste0("r$hist_nll_spabund_bt__model_sqsum[", i, ", '1994-02']"))
+}
+ok(ut_cmp_equal(
+    r$hist_nll_spabund_bt__model_sum,
+    ifelse(r$hist_nll_spabund_bt__model_n > 0, r$hist_nll_spabund_bt__model_sum[,"1994-02"], 0),
+    end = NULL), "hist_nll_spabund_bt__model_sum: History is the same shape as __model_n")
+ok(gadget3:::ut_cmp_df(as.data.frame(r$hist_nll_spabund_bt__model_n), '
+1990-01 1990-02 1991-01 1991-02 1992-01 1992-02 1993-01 1993-02 1994-01 1994-02
+      1       1       1       1       1       1       1       1       1       1
+      0       1       1       1       1       1       1       1       1       1
+      0       0       1       1       1       1       1       1       1       1
+      0       0       0       1       1       1       1       1       1       1
+      0       0       0       0       1       1       1       1       1       1
+      0       0       0       0       0       1       1       1       1       1
+      0       0       0       0       0       0       0       0       1       1
+      0       0       0       0       0       0       0       0       0       1
+'), "hist_nll_spabund_bt__model_n: Added value once per step, apart from 1993")
+ok(ut_cmp_equal(
+    r$hist_nll_spabund_bt__obs_mean,
+    array(
+        obs_linreg_df$mean,
+        dim = c(vec = length(obs_linreg_df$mean), time = 10),
+        dimnames = list(vec = NULL, time = paste0(rep(1990:1994, each=2), c("-01", "-02"))) ),
+    end = NULL), "hist_nll_spabund_bt__obs_mean: Repeats the input observations")
+nll_mean <- r$hist_nll_spabund_bt__model_sum[, '1994-02']/r$hist_nll_spabund_bt__model_n[, '1994-02']
+ok(ut_cmp_equal(
+    r$nll_spabund_bt__nll,
+    gadget3:::regression_linear(nll_mean, obs_linreg_df$mean, NaN, 1) ), "r$nll_spabund_bt__nll: Matches derived version")
+
+# cs_model
+ok(gadget3:::ut_cmp_df(as.data.frame(r$hist_nll_spabund_cs_model__model_n), '
+1990-01 1990-02 1991-01 1991-02 1992-01 1992-02 1993-01 1993-02 1994-01 1994-02
+      9      18      18      18      18      18      18      18      18      18
+      0       0       0       0       9      18      18      18      18      18
+      0       0       0       0       0       0       9      18      18      18
+'), "hist_nll_spabund_cs_model__model_n: 18 data points (step,age,length) in each cell, gathered per year")
+expected_m <- r$hist_stst__num * c(r$hist_stst__wgt * rep(3:5, each = 3))
+for (i in seq_len(nrow(obs_ss_df))) {
+    # Pick out all relevant measurements to this row
+    x <- expected_m[,,time = paste0(obs_ss_df[i, "year"], c("-01", "-02"))]
+    ok(ut_cmp_equal(
+        as.vector(r$hist_nll_spabund_cs_model__model_sum[i, '1994-02']),
+        sum(x) ), paste0("r$hist_nll_spabund_cs_model__model_sum[", i, ", '1994-02']"))
+    ok(ut_cmp_equal(
+        as.vector(r$hist_nll_spabund_cs_model__model_sqsum[i, '1994-02']),
+        sum(x**2) ), paste0("r$hist_nll_spabund_cs_model__model_sqsum[", i, ", '1994-02']"))
+}
+nll_var <- r$hist_nll_spabund_cs_model__model_sqsum[, '1994-02']/r$hist_nll_spabund_cs_model__model_n[, '1994-02'] -
+    (r$hist_nll_spabund_cs_model__model_sum[, '1994-02']/r$hist_nll_spabund_cs_model__model_n[, '1994-02'])**2
+nll_mean <- r$hist_nll_spabund_cs_model__model_sum[, '1994-02']/r$hist_nll_spabund_cs_model__model_n[, '1994-02']
+ok(ut_cmp_equal(
+    r$nll_spabund_cs_model__nll,
+    (nll_mean - obs_ss_df$mean)^2 * 1/nll_var), "r$nll_spabund_cs_model__nll: Matches derived version")
+
+# Overall nll
+ok(ut_cmp_equal(nll,
+    r$nll_spabund_bt__nll[[1]] +
+    sum(r$nll_spabund_cs_model__nll) +
+    0 ), "nll: Overall value matches")
+
+gadget3:::ut_tmb_r_compare2(model_fn, model_cpp, params)
